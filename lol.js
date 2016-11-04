83047c9a9511fa2537576ce96027b4232954abbd
Student <idk123@gmail.com>
2016-11-04 09:18:54 -1000

    new file:   lol.js

---
+++ lol.js
@@ -0,0 +1,50 @@
+var hero = "Deadpool"
+var gender = "Female"
+var age = "42"
+var weapon = "Boomstick McBoom"
+var pronoun = null
+var xp = null
+
+if (gender === "male"){
+	pronoun = "his";
+} else if (gender === "female"){
+	pronoun = "her";
+} else{
+	pronoun = "their";
+}
+
+if (age < 18){
+	xp = "is just beginning";
+}
+else if (age > 21 && age < 40){
+	xp = "has seen a few battles";
+}
+else (age > 40);{
+	xp = "has lived through many epic battles";
+}
+console.log(hero + " is our lovable hero, who " + xp + " with his trusty " + weapon);
+
+//1 Fish 2 Fish
+var count;
+var animal = "cheetah";
+function animalHouse (Ani1, Ani2){
+	if (Ani1 + Ani2){
+		count = 3;
+		console.log(count);
+	}
+}
+animalHouse(1237846123894612, 6785123465129)
+
+if (count > 2){
+	console.log("We have too many " + animal + "s");
+}
+else{
+	console.log("Welcome to the club");
+}
+
+//EWW AVENGERS
+var hero = "Irunman"
+
+function avengersAssemble (hi){
+	if(hi == )
+}
